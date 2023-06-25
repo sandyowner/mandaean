cad4321280830eb0698c05bb7c10aa255bf8777b
sandyowner <sandeep872648@gmail.com>
2023-06-25 18:06:04 +0530

    otp

--- resources/views/emails/otp.blade.php
+++ resources/views/emails/otp.blade.php
@@ -1,3 +1,2 @@
 <h1>Hi, {{ $name }}</h1>
-<p>Here's your One Time Password to your Mandaean account.</p>
-<h1>{{ $otp }}</h1>
+<p>Here's your One Time Password <strong>{{ $otp }}</strong> to your Mandaean account.</p>
