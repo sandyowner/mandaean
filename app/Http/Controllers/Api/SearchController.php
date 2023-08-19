d5548189bb567102488c307d0bc6c2879e9df3b4
sandyowner <sandeep872648@gmail.com>
2023-08-19 17:17:57 +0530

    search

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Mandanism;
 use Validator;
 use Auth;
 
@@ -23,11 +25,41 @@ class SearchController extends Controller
         }
 
         $id = Auth::id();
+        $searchArray = [];
+        $search = $request->search;
+        $product = Product::where('status','active')
+            ->where('name', 'LIKE', '%'.$search.'%')
+            ->get();
 
-        return response([
-            'status'=>true,
-            'message'=>'Data fetched.',
-            'data'=>[]
-        ],201);
+        foreach ($product as $key => $value) {
+            $searchArray['id'] = $value->id;
+            $searchArray['name'] = $value->name;
+            $searchArray['section'] = 'product';
+        }
+
+        $mandanism = Mandanism::where('title', 'LIKE', '%'.$search.'%')
+                // ->orWhere('group', 'LIKE', '%'.$search.'%')
+                // ->orWhere('description', 'LIKE', '%'.$search.'%')
+                ->get();
+
+        foreach ($mandanism as $key1 => $value1) {
+            $searchArray['id'] = $value1->id;
+            $searchArray['name'] = $value1->title;
+            $searchArray['section'] = 'mandanism category';
+        }
+
+        if(count($searchArray)>0){
+            return response([
+                'status'=>true,
+                'message'=>'Data fetched.',
+                'data'=>$searchArray
+            ],201);
+        }else{
+            return response([
+                'status' => false,
+                'message' => 'No search found.',
+                'data' => []
+            ],422);
+        }
     }
 }
