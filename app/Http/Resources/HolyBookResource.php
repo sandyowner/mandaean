b1fc24caaf148e3f6ee2dbb58a69fada0f030d5c
sandyowner <sandeep872648@gmail.com>
2023-08-28 01:19:07 +0530

    changes

--- app/Http/Resources/HolyBookResource.php
+++ app/Http/Resources/HolyBookResource.php
@@ -17,34 +17,66 @@ class HolyBookResource extends JsonResource
     public function toArray($request): array
     {
         $id = Auth::id();
+        $type = $request->type;
         $book = Bookmark::where(['user_id'=>$id,'book_id'=>$this->id])->first();
-        if($request->lang == 'ar'){
-            return [
-                'id'            => $this->id,
-                'title'         => $this->ar_title,
-                'description'   => $this->ar_description,
-                'image'         => url('/').'/'.$this->image,
-                'url'           => url('/').'/'.$this->url,
-                'bookmark'      => ($book)?'yes':'no',
-            ];
-        }elseif ($request->lang == 'pe') {
-            return [
-                'id'            => $this->id,
-                'title'         => $this->pe_title,
-                'description'   => $this->pe_description,
-                'image'         => url('/').'/'.$this->image,
-                'url'           => url('/').'/'.$this->url,
-                'bookmark'      => ($book)?'yes':'no',
-            ];
+        if($type=='holy'){
+            if($request->lang == 'ar'){
+                return [
+                    'id'            => $this->id,
+                    'title'         => $this->ar_title,
+                    'description'   => $this->ar_description,
+                    'image'         => ($this->image)?url('/').'/'.$this->image:null,
+                    'url'           => ($this->url)?url('/').'/'.$this->url:null,
+                    'bookmark'      => ($book)?'yes':'no',
+                ];
+            }elseif ($request->lang == 'pe') {
+                return [
+                    'id'            => $this->id,
+                    'title'         => $this->pe_title,
+                    'description'   => $this->pe_description,
+                    'image'         => ($this->image)?url('/').'/'.$this->image:null,
+                    'url'           => ($this->url)?url('/').'/'.$this->url:null,
+                    'bookmark'      => ($book)?'yes':'no',
+                ];
+            }else{
+                return [
+                    'id'            => $this->id,
+                    'title'         => $this->title,
+                    'description'   => $this->description,
+                    'image'         => ($this->image)?url('/').'/'.$this->image:null,
+                    'url'           => ($this->url)?url('/').'/'.$this->url:null,
+                    'bookmark'      => ($book)?'yes':'no',
+                ];
+            }
         }else{
-            return [
-                'id'            => $this->id,
-                'title'         => $this->title,
-                'description'   => $this->description,
-                'image'         => url('/').'/'.$this->image,
-                'url'           => url('/').'/'.$this->url,
-                'bookmark'      => ($book)?'yes':'no',
-            ];
+            if($request->lang == 'ar'){
+                return [
+                    'id'            => $this->id,
+                    'title'         => $this->other_ar_title,
+                    'description'   => $this->other_ar_description,
+                    'image'         => ($this->other_image)?url('/').'/'.$this->other_image:null,
+                    'url'           => ($this->other_url)?url('/').'/'.$this->other_url:null,
+                    'bookmark'      => ($book)?'yes':'no',
+                ];
+            }elseif ($request->lang == 'pe') {
+                return [
+                    'id'            => $this->id,
+                    'title'         => $this->other_pe_title,
+                    'description'   => $this->other_pe_description,
+                    'image'         => ($this->other_image)?url('/').'/'.$this->other_image:null,
+                    'url'           => ($this->other_url)?url('/').'/'.$this->other_url:null,
+                    'bookmark'      => ($book)?'yes':'no',
+                ];
+            }else{
+                return [
+                    'id'            => $this->id,
+                    'title'         => $this->other_title,
+                    'description'   => $this->other_description,
+                    'image'         => ($this->other_image)?url('/').'/'.$this->other_image:null,
+                    'url'           => ($this->other_url)?url('/').'/'.$this->other_url:null,
+                    'bookmark'      => ($book)?'yes':'no',
+                ];
+            }
         }
     }
 }
